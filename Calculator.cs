a31a265a6b1c3476b537b9f618a158f3349eabdc
Bence Molnár <molnarbence@users.noreply.github.com>
2023-12-01 20:45:29 +0000

    Initial commit

---
+++ Calculator.cs
@@ -0,0 +1,13 @@
+﻿namespace curvature;
+
+public static class Calculator
+{
+    public static double CalculateDifference(double sectionLength, double radius)
+    {
+        double quarterCurvature = 2 * radius * Math.PI / 4;
+        double angle = sectionLength / quarterCurvature * 90;
+        double leg = Math.Cos(angle * Math.PI / 180) * radius;
+        double difference = radius - leg;
+        return difference;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using curvature;
+
+const double radius = 6_378_000;
+
+// prompt the section length in meters
+Console.Write("Enter the section length in meters: ");
+double sectionLength = double.Parse(Console.ReadLine() ?? string.Empty);
+
+double difference = Calculator.CalculateDifference(sectionLength, radius);
+Console.WriteLine($"Difference: {difference,10:F3} meters");
+
