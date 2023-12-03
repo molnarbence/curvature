56968eb1cda37a3872e2a6d7ff7829801f2743ce
Bence Molnár <xjarekx@gmail.com>
2023-12-03 14:57:47 +0100

    Add in F#

---
+++ src/CalculateCurvature/Program.cs
@@ -0,0 +1,21 @@
+﻿const double radius = 6_378_000;
+// get section length from input parameter
+double sectionLength = double.Parse(args[0]);
+
+// write the result to the console
+var curvature = CalculateCurvature(sectionLength, radius);
+// write in meters with 2 decimal places and some padding
+Console.WriteLine($"Radius: {radius / 1000,16:F2} km");
+Console.WriteLine($"Section length: {sectionLength / 1000,8:F2} km");
+// add a line of dashes
+Console.WriteLine(new string('-', 40));
+Console.WriteLine($"Curvature {curvature,14:F2} m");
+
+static double CalculateCurvature(double sectionLength, double radius)
+{
+    double quarterCurvature = 2 * radius * Math.PI / 4;
+    double angle = sectionLength / quarterCurvature * 90;
+    double leg = Math.Cos(angle * Math.PI / 180) * radius;
+    double difference = radius - leg;
+    return difference;
+}
---
+++ src/CurvatureFuntion/Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+
+// earth radius in meters
+let earthRadius = 6_371_000.0
+
+let calculateDeviation (sectionLength, radius) =
+    let quarterCircleLength = Math.PI * radius / 2.0
+    let angle = (sectionLength / quarterCircleLength) * (Math.PI / 2.0)
+    let leg = radius * Math.Cos(angle)
+    radius - leg
+
+// get section length from input argument and parse it as float
+let sectionLengthInMeters = Environment.GetCommandLineArgs().[1] |> float
+
+let deviation = calculateDeviation(sectionLengthInMeters, earthRadius)
+
+printfn $"Deviation: {deviation,10:F2} m"
